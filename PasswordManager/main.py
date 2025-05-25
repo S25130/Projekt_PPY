b10bc8b9da8782b023c11887be0f680e7bf76ce5
S25130 <s25130@pjwstk.edu.pl>
2025-05-25 15:14:33 +0200

    Update main.py

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -5,7 +5,7 @@ import tkinter as tk
 def main():
     root = tk.Tk()
     root.title("PasswordManager")
-    root.geometry("600x300")  # szerokość x wysokość
+    root.geometry("1200x675")  # szerokość x wysokość
 
     label = tk.Label(root, text="Welcome in your Password Manager!", font=("Arial", 14))
     label.pack(pady=20)
