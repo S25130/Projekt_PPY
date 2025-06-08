87ed989459062cfd046bd2ebf083e35f3017ce95
S25130 <s25130@pjwstk.edu.pl>
2025-06-08 21:06:20 +0200

    update

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,11 +1,16 @@
-# main.py
-
 import tkinter as tk
+import os
 
 def main():
     root = tk.Tk()
     root.title("PasswordManager")
-    root.geometry("1200x675")  # szerokość x wysokość
+    root.geometry("1200x675")
+
+    try:
+        icon = tk.PhotoImage(file="locker.png")
+        root.iconphoto(True, icon)
+    except tk.TclError:
+        print("Błąd: Nieprawidłowy plik PNG lub plik nie istnieje.")
 
     label = tk.Label(root, text="Welcome in your Password Manager!", font=("Arial", 14))
     label.pack(pady=20)
